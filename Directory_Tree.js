37ca4c19d0b3176de4e3a81346a4d0181a68561f
shivam kumar <shivamkumar789321@gmail.com>
2025-10-18 03:38:54 +0530

    TailwindCSS_Implemented

---
+++ Directory_Tree.js
@@ -0,0 +1,20 @@
+/*
+DexKor_Task
+ ┣ node_modules/
+ ┣ public
+ ┃ ┗ vite.svg
+ ┣ src
+ ┃ ┣ App.tsx
+ ┃ ┣ index.css
+ ┃ ┗ main.tsx
+ ┣ .gitignore
+ ┣ eslint.config.js
+ ┣ index.html
+ ┣ package-lock.json
+ ┣ package.json
+ ┣ README.md
+ ┣ tsconfig.app.json
+ ┣ tsconfig.json
+ ┣ tsconfig.node.json
+ ┗ vite.config.ts
+*/
