06d5a5aa4a40bb64664c58ce23ce27f74e8ea55d
shivam kumar <shivamkumar789321@gmail.com>
2025-10-19 21:15:05 +0530

    Font_IBM_Plex_Sans_Updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import '@fontsource/ibm-plex-sans/300.css'; // Light
+import '@fontsource/ibm-plex-sans/400.css'; // Regular
+import '@fontsource/ibm-plex-sans/500.css'; // Medium
+import '@fontsource/ibm-plex-sans/600.css'; // Semi-Bold
+import '@fontsource/ibm-plex-sans/700.css'; // Bold
+
 import './index.css'
 import App from './App.tsx'
 
